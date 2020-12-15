9d6c1b2a384b3488285f92cf76af42af74c569f0
svetlyobg <swk_12@abv.bg>
2020-12-15 09:15:04 +0200

    Form1 - Maximize box - False

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -298,6 +298,7 @@ namespace Chocolatey
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBox2);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Chocolatey";
